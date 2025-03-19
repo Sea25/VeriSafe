fbb983a839035a89ba569d2b7dfb38421ca361bd
SIDDHARTH <siddhubhai998@gmail.com>
2025-03-19 15:30:39 +0530

    Update script.js

---
+++ frontend/javascript/script.js
@@ -0,0 +1,5 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Add login logic here
+    window.location.href = 'face_verify.html';
+});
